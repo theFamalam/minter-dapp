c014fc0d3e094e5b5e974c333cc172d2830873a7
Harry Roberts <harryaroberts@sky.com>
2022-03-23 23:30:44 +0000

    rinkeby tester

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xFC51D211deBD76c85021392ffC8130183eEDC3Ac"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
