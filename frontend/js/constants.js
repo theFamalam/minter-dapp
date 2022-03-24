db1c5b7405ae3dc64e6c1514a0b780c553dcc055
Harry Roberts <harryaroberts@sky.com>
2022-03-24 05:32:53 +0000

    updatings

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -11,15 +11,15 @@ const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
 const p_presale_coming_soon = "We are working hard to launch our first NFT Collection. Stay tuned for updates!";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
+const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale!";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-const p_public_mint = "No whitelist needed. Public minting is now Live! 🎉";
+const p_public_mint = "No whitelisting necessary. Mint your Orca now!";
 const button_presale_coming_soon = "Get on the Whitelist";
 const button_presale_mint_whitelisted = "Mint Your Orca NFT";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
-const mint_failed = "Minting failed. 😢 Please try again.";
+const button_public_mint = "Mint Your Orca";
+const mint_failed = "Minting failed. Please try again.";
 
 const abi = [
   {
